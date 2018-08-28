ee0b5d35f014c7b1d735fccbd1b7ad64efc19dd1
Raymond Korir <raymond.korir@uonbi.ac.ke>
2018-08-28 23:18:46 +0300

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,16 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
-  def self.set.set_options(params, session)
+  def self.movies(filters,sort_field)
+  	return self.order(sort_field) if not filters
+  	self.where({:rating => filters.keys}).order(sort_field)
+  end
+
+  def self.ratings
+  	self.pluck(:rating).uniq
+  end
+
+  def self.set_options(params, session)
   	setup = {}
 
   	setup[:ratings] = if params[:ratings]
@@ -21,4 +30,6 @@ class Movie < ActiveRecord::Base
   	  nil   
    end
 
+   setup
+  end
 end
