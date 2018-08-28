a165d6048bf6808999e10635bbd67585cfcbb6a3
Raymond Korir <raymond.korir@uonbi.ac.ke>
2018-08-28 19:14:30 +0300

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
 
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,16 +12,17 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    @checked_ratings = check
-    @checked_ratings.each do |rating|
-      params[rating] = true
-    end
-
-    if params[:sort]
-      @movies = Movie.order(params[:sort])
+    #Comment for part 1 complete
+    if params[:title] == "sort"
+      @movies = Movie.all.order(:title => "ASC")
+      @title_header_class= "hilite"
+    elsif params[:release_date] == "sort"
+      @movies = Movie.all.order(:release_date => "DESC")
+      @release_date_class="hilite"
+      
     else
-      @movies = Movie.where(:rating => @checked_ratings)
+      @movies = Movie.all
+      
     end
   end
 
@@ -25,7 +31,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -36,7 +42,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -46,16 +52,6 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
-  end
-
-  private
-
-  def check
-    if params[:ratings]
-      params[:ratings].keys
-    else
-      @all_ratings
-    end
   end
 
 end
