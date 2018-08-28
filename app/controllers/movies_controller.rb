ef0c82f6a9f6102eff7e85bbf6708354e407dd8a
Raymond Korir <raymond.korir@uonbi.ac.ke>
2018-08-28 22:59:46 +0300

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,10 +7,16 @@ class MoviesController < ApplicationController
   end
 
   def index
+    setup = Movies.set_options(params, session)
+
+
     @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
     @checked_ratings = check
     @checked_ratings.each do |rating|
       params[rating] = true
+    session[:ratings] = setup[:ratings]
+    session[:order_by] = setup[:order_by]
+
     end
 
     if params[:sort]
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,25 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+  def self.set.set_options(params, session)
+  	setup = {}
+
+  	setup[:ratings] = if params[:ratings]
+  		params[:ratings]
+  	elsif session[:ratings]
+       session[:ratings]
+  	else
+  	  self.ratings     
+   end
+
+   setup[:order_by] = if params[:order_by]
+  		params[:order_by]
+  	elsif session[:order_by]
+       session[:order_by]
+  	else
+  	  nil   
+   end
+
+   setup
+  end
 end
